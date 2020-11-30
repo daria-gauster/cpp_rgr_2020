712c6a8a532ac2a43eb15df40e1880f795066584
adamska <dooody94@gmail.com>
2020-11-30 22:11:40 +0200

    remove gitignore files

---
+++ backend/data.js
@@ -0,0 +1,43 @@
+export default {
+books:[
+    {
+        _id: '1',
+        title: 'Gadsby',
+        image: '/images/gadsby.jpg',
+        category: 'Fiction',
+        genre: 'Historical',
+        author: 'Ernest Vincent Wright',
+        published: '01-03-1997',
+        price: 2.5,
+        rating: 4.5,
+        numReviews: 10,
+        countInStock: 6,
+    },
+    {
+        _id: '2',
+        title: 'The Mind Test',
+        image: '/images/mind_test.jpg',
+        category: 'Nonfiction',
+        genre: 'Psychology',
+        author: 'Rita Aero',
+        published: '01-09-1981',
+        price: 5,
+        rating: 3.5,
+        numReviews: 11,
+        countInStock: 0,
+    },
+    {
+        _id: '3',
+        title: 'Don Quixote',
+        image: '/images/don.jpg',
+        category: 'Fiction',
+        genre: 'Adventure',
+        author: 'Miguel de Cervantes Saavedra',
+        published: '01-03-1605',
+        price: 4.45,
+        rating: 3.8,
+        numReviews: 211558,
+        countInStock: 50,
+    },
+    ]
+}
